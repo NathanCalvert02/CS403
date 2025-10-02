de301a332ac59530015502ef23d4076f650aeef7
NathanCalvert02 <ncalvert@wvup.edu>
2025-10-01 20:03:01 -0400

    [200~Modified Binary Search

---
+++ Leet Code Patterns/ModifiedBinarySearch/ModifiedBinarySearch/ModifiedBinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+        {
+            return false;
+        }
+
+        int rows = matrix.Length;
+        int columns = matrix[0].Length;
+
+        //Start from the top-right corner
+        int row = 0;
+        int column = columns - 1;
+
+        while (row < rows && column >= 0)
+        {
+            if (matrix[row][column] == target)
+            {
+                return true;
+            }
+            else if (matrix[row][column] > target)
+            {
+                //If the target is smaller, move left
+                column--;
+            }
+            else
+            {
+                //If the target is larger, move down
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
